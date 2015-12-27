3569e2211807c20b478df044e740733847276faf
James Bowman <jamesb@excamera.com>
2015-12-26 22:01:20 -0800

    displaying lines

--- te.fs
+++ te.fs
@@ -1,3 +1,5 @@
+4 constant columns
+
 create ff       256 64 * allot 
 : line  ( n -- a u )    64 * ff + 64 ;
 create linebuf  65 allot
@@ -9,8 +11,9 @@ create linebuf  65 allot
 : bg            csi ." 48;5;" n. ." m" ;
 
 form constant w 1- constant h
-w 4 / constant qw
-0 value ml  0 value lnum
+w columns / constant qw
+0 value nl  0 value lnum
+: ml ( -- maxline ) nl 1- ;
 
 : ld  ( filename -- )
     ff 256 64 * blank
@@ -20,24 +23,61 @@ w 4 / constant qw
         linebuf 65 blank
         linebuf 65 r@ read-line throw
     while
+        ( lnum n )
         drop ( width not needed )
         linebuf over line move
         1+
     repeat
-    drop to ml
-    ml . ."  lines read" cr
+    drop to nl
+    nl . ."  lines read" cr
     r> drop
 ;
 
-: show
+: redraw
+    utime
     page
-    ml 0 do
+    nl 0 do
         i h / qw * i h mod at-xy
         i lnum = if 100 else 18 then fg 
         i 1+ 3 .r normal space
         i line type
     loop
-    0 h 1- at-xy
+    0 h at-xy
+    utime 2swap d- d. lnum . .s
+;
+
+: go    0 max ml min to lnum ;
+: up    1 max lnum swap - go ; 
+: down  1 max lnum + go ; 
+
+: visual
+    0
+    begin
+        redraw
+        ekey ekey>char if ( c )
+            dup '0' '9' 1+ within if
+                '0' - swap 10 * +
+            else
+                case
+                13      of down endof
+                'G'     of dup 0= nl and + 1- go endof
+                'k'     of up endof
+                'j'     of down endof
+                'q'     of exit endof
+                27      of exit endof
+                        . abort
+                endcase 0
+            then
+        else ekey>fkey if ( key-id )
+            case
+            k-up    of up endof
+            k-down  of down endof
+                    drop
+            endcase 0
+        else ( keyboard-event )
+        drop \ just ignore an unknown keyboard event type
+        then then
+    again
 ;
 
 : .c
@@ -56,4 +96,5 @@ w 4 / constant qw
 
     cr 256 232 do i .c loop
 ;
-s" 8080.fs" ld show
+
+s" 8080.fs" ld   visual
